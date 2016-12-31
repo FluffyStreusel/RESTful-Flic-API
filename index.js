3bc1d3bc5e68f5206d5ea587b1dc4e9b1548d80e
Micah Ayers <FluffyStreusel@users.noreply.github.com>
2016-12-30 23:17:25 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,6 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
 var boolValue = false;
 
 var statuses;
@@ -41,5 +35,5 @@ app.post('/btn/press', function(req, res) {
 
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('Flic RESTful API is running on port ', app.get('port'), '.');
 });
