d55285b86f25a5713f5e1e763322a1fc2af06f0c
Micah Ayers <FluffyStreusel@users.noreply.github.com>
2017-01-02 18:29:45 -0600

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.get(apiDirectory + "/btn/:id", function(req, res) {
 });
 
 app.get(apiDirectory + "/btn/:id/wpointer", function(req, res) {
-  res.send(buttonIdentifiers[req.params.id]['WebPointer']);
+  res.json(buttonIdentifiers[req.params.id]['WebPointer']);
 });
 
 app.get(apiDirectory + '/btn/status', function(req, res) {
